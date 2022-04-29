eb845cc2695f391e3ab8a166d9c3dece97923d88
Stella Ji <stellaji0823@gmail.com>
2022-04-29 14:54:44 -0700

    Update skillDemoTest.java

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(5, skillDemo.multiply(1, 2)); //This test shoudl fail
+        assertEquals(5, skillDemo.multiply(1, 5)); //This test should pass
     }
 }
+g
